739dc853330157a5fbf9c7db80923b4a81bf2980
Veeresh603 <veereshkavalagi31@gmail.com>
2021-03-16 20:31:49 +0530

    carousel responsive

--- src/components/OurWork/index.js
+++ src/components/OurWork/index.js
@@ -1,12 +1,11 @@
 import React from "react"
 import styled from "styled-components"
-import Swiper from "react-id-swiper"
 import "swiper/css/swiper.css"
-import Slider from "react-slick";
- 
+import Slider from "react-slick"
+
 // Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const data = [
   {
@@ -61,15 +60,15 @@ const data = [
 
 function courseone() {
   var settings = {
-
     dots: true,
     infinite: true,
-    autoplay:true,
+    autoplay: true,
     centerPadding: "10px",
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     initialSlide: 0,
+    
     responsive: [
       {
         breakpoint: 1024,
@@ -77,145 +76,133 @@ function courseone() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
-        breakpoint: 600,
+        breakpoint: 767,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
- 
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+
   return (
     <Wrapper>
       <h2 className="borderLeftRight">our work</h2>
       <Slider {...settings}>
         {data.map(d => {
-          return(
+          return (
             <div className="first">
-            <img src={d.img} alt="" />
-            <div className="text">
-              <h4>{d.h3}</h4>
+              <img src={d.img} alt="" />
+              <div className="text">
+                <h4>{d.h3}</h4>
+              </div>
             </div>
-          </div>
           )
         })}
-         
-        
-        </Slider>
-   
-  
+      </Slider>
     </Wrapper>
-    
-    // <Wrapper>
-    //     <Text>
-    //     <h2>our work</h2>
-    //     </Text>
-
-    //   <Swiper className="swiper" {...params}>
-    //     {data.map(d => (
-    //       <Wrapper>
-    //         <img src={d.img} alt="" />
-    //         <Text01>
-    //         <h3>{d.h3}</h3>
-    //         </Text01>
-
-    //       </Wrapper>
-    //     ))}
-    //   </Swiper>
-    // </Wrapper>
   )
 }
 
 export default courseone
 
 const Wrapper = styled.div`
-margin-top:200px;
- width:90%;
- height:auto;
+  margin-top: 200px;
+  width: 90%;
+  height: auto;
   padding: 40px;
-.first{
-  width:370px;
-  height:auto;
-}
-.first:hover{
-     h4{
-       background-color: var(--primaryColor);
-       color: white;
-       transition: 1s ease-in-out;
-     }
-}
-.text{
-  width: 100%;
-  color:white;
-  display: flex;
-  justify-content: flex-end;
-}
-img{
-  width:100%;
-  height:auto;
-}
-h4{
-  width:300px;
-  background-color: #f8f8f8;
-      box-shadow: 0px 2px 8px rgba(0,0,0,0.25);
-      color:#111111;
-  height:auto;
-  padding:20px;
-  margin-top:-25px; 
-  font-size: 15px;
-}
-h2{
-  color: var(--primaryColor);
-  font-size: 25px;
-  font-weight: 600;
-  margin-bottom:25px;
-}
-.slick-slide img {
-  margin:auto;
-  align-self: center;
-}
-.slick-slide{
-  margin-left:20px;
-}
-.slick-track{
-  display:flex;
-  align-items:center;
-}
-.borderLeftRight {
-      display: inline-block;
-      position: relative;
-      cursor: pointer;
+  @media (max-width:479px){
+    margin:0px;
+  }
+  @media (max-width:767px){
+    margin:0px;
+  }
+  .first {
+    width: 370px;
+    height: auto;
+  }
+  .first:hover {
+    h4 {
+      background-color: var(--primaryColor);
+      color: white;
+      transition: 1s ease-in-out;
     }
+  }
+  .text {
+    width: 100%;
+    color: white;
+    display: flex;
+    justify-content: flex-end;
+  }
+  img {
+    width: 100%;
+    height: auto;
+  }
+  h4 {
+    width: 300px;
+    background-color: #f8f8f8;
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.25);
+    color: #111111;
+    height: auto;
+    padding: 20px;
+    margin-top: -25px;
+    font-size: 15px;
+    @media (max-width:991px){
+      font-size: 10px;
+      width: calc(300px - 130px);
+      height: auto;
+    }
+  }
+  h2 {
+    color: var(--primaryColor);
+    font-size: 25px;
+    font-weight: 600;
+    margin-bottom: 25px;
+  }
+  .slick-slide img {
+    margin: auto;
+    align-self: center;
+  }
+  .slick-slide {
+    margin-left: 20px;
+  }
+  .slick-track {
+    display: flex;
+    align-items: center;
+  }
+  .borderLeftRight {
+    display: inline-block;
+    position: relative;
+    cursor: pointer;
+  }
 
-    .borderLeftRight::after {
-      content: "";
-      position: absolute;
-      width: 100%;
-      transform: scaleX(0);
-      height: 2px;
-      bottom: 0;
-      left: 0;
-      background-color: hsl(243, 80%, 62%);
-      transform-origin: bottom right;
-      transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
-    }
+  .borderLeftRight::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: hsl(243, 80%, 62%);
+    transform-origin: bottom right;
+    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+  }
 
-    .borderLeftRight:hover::after {
-      transform: scaleX(1);
-      transform-origin: bottom left;
-    }
+  .borderLeftRight:hover::after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
 `
-
