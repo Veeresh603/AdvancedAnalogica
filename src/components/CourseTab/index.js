117c48281a772ca873f242776736f61542bca829
Veeresh603 <veereshkavalagi31@gmail.com>
2021-03-24 13:01:54 +0530

    hhf

--- src/components/CourseTab/index.js
+++ src/components/CourseTab/index.js
@@ -66,7 +66,7 @@ export default function CourseTab(props) {
   const [value, setValue] = React.useState(0);
  
   const disqusConfig = {
-    shortname:"analogica3",
+    shortname:"analogica",
     config:{identifier : id, slug , title,}
      
      
